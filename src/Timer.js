550a89e18d027960c8d8de4cf2c1ba826d502e5f
Chely Ho <adept_inept@yahoo.com>
2018-09-25 21:57:16 +0000

    automatically backed up by learn

--- src/Timer.js
+++ src/Timer.js
@@ -15,7 +15,6 @@ class Timer extends Component {
 
   componentDidUpdate() {
     this.timer.current.style.color = '#'+Math.floor(Math.random()*16777215).toString(16)
-
   }
 
   shouldComponentUpdate() {
